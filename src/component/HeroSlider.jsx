5b791199b5519c59cf03f96393fb6a4c9b822bfc
Azizul Hakim <azizulhakimgps@gmail.com>
2023-11-24 21:16:04 +0600

    made HeroSlider component responsive

--- src/component/HeroSlider.jsx
+++ src/component/HeroSlider.jsx
@@ -16,13 +16,13 @@ const HeroSlider = () => {
       {data.map((item) => {
         return (
           <SwiperSlide>
-            <div className="w-full bg-[#ffffff]">
-              <div className="flex items-center gap-10 justify-center md:h-[500px] md:w-[1152px] mx-auto">
-                <img src={item.image} alt="" className="w-[400px]" />
+            <div className="w-full bg-[#ffffff] px-2 md:px-0">
+              <div className="flex md:flex-row flex-col items-center gap-10 justify-center md:h-[500px] h-auto md:w-[1152px] mx-auto">
+                <img src={item.image} alt="" className="md:w-[400px] w-full" />
                 <span className="flex flex-col gap-5">
-                  <p className="text-2xl">{item.description}</p>
-                  <p className="text-8xl text-zinc-900">{item.heading}</p>
-                  <p className="text-3xl text-zinc-600 text-justify">{item.subheading}</p>
+                  <p className="text-2xl md:text-start text-center">{item.description}</p>
+                  <p className="text-8xl text-zinc-900 md:text-start text-center">{item.heading}</p>
+                  <p className="text-3xl text-zinc-600 text-justify md:px-0 px-2">{item.subheading}</p>
                 </span>
               </div>
             </div>
