b7c51409eadff79b23a73b759d3c9978324135c1
Azizul Hakim <azizulhakimgps@gmail.com>
2023-11-24 21:46:15 +0600

    added offer tab and fixed some bugs of offer tab, made responsive

--- src/component/offerTab.jsx
+++ src/component/offerTab.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const offerTab = () => {
+const OfferTab = () => {
   return (
-    <div>offerTab</div>
+    <div className='bg-green-700 w-full '>
+      <div className="md:w-[1152px] w-full mx-auto md:h-[150px] h-[300px] flex md:flex-row flex-col md:justify-between md:px-0 px-3 justify-center md:gap-0 gap-7 items-center">
+        <div>
+          <h2 className='uppercase text-5xl text-white'>purchase data</h2>
+          <p className='uppercase text-2xl text-white'>Pre-paid Data Credits</p>
+          <p className='uppercase text-2xl text-white'>Get up to 20% Bonus Credit on Data Purchases</p>
+        </div>
+        <p className='bg-[#13bf47] cursor-pointer text-white md:text-4xl text-2xl font-bold px-24 py-3 rounded'>get 20% bonus</p>
+      </div>
+    </div>
   )
 }
 
-export default offerTab
+export default OfferTab
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../component/Layout'
 import HeroSlider from '../component/HeroSlider'
+import OfferTab from '../component/offerTab'
 
 const Homepage = () => {
   return (
     <Layout>
         <HeroSlider/>
+        <OfferTab/>
     </Layout>
   )
 }
